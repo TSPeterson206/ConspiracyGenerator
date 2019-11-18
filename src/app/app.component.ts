67899e095e036f190fc07f550fd8be0cdeb2a185
TOBY PETERSON <tspeterson206@gmail.com>
2019-11-17 22:21:53 -0800

    added random generation of all 3 elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,14 @@ import { } from './person/person.component'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // thePerson=person;
   title = 'conspiracyGenerator';
 
+  person=['Alex Jones','Hillary Clinton','Bill Clinton','The Illuminati','The federal government','Conor Mcgregor','Jeff Bezos','Donald Trump','The Magnificent Twelve','The FBI','Michael Jackson','R Kelly','Jay Inslee','Vincent D\'onofrio','Alexander Graham Bell','Our founding father','Captain James Tiberius Kirk'];
+  tempPerson=[];
+  
   generateTheory(){
-    
+    const rando = Math.round(Math.random()*10);
+    this.tempPerson=[this.person[rando]];
+    console.log(rando,'appcomponent', [this.person[rando]]);
   }
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent implements OnInit {
-person=['Alex Jones','Hillary Clinton','Bill Clinton','The Illuminati','The federal government','Conor Mcgregor','Jeff Bezos','Donald Trump','The Magnificent Twelve','The FBI','Michael Jackson','R Kelly','Jay Inslee','Vincent D\'onofrio','Alexander Graham Bell','Our founding father','Captain James Tiberius Kirk'];
-  tempPerson='';
+// person=['Alex Jones','Hillary Clinton','Bill Clinton','The Illuminati','The federal government','Conor Mcgregor','Jeff Bezos','Donald Trump','The Magnificent Twelve','The FBI','Michael Jackson','R Kelly','Jay Inslee','Vincent D\'onofrio','Alexander Graham Bell','Our founding father','Captain James Tiberius Kirk'];
+  testPerson='';
+
+  @Input() person: string[];
+  @Input() tempPerson: string[];
+  
+
+
 constructor() { 
-  // person=this.person;
   }
+
   ngOnInit() {
   }
 
   generatePerson(){
     const rando = Math.round(Math.random()*10);
-    this.tempPerson=this.person[rando];
-    console.log(rando)
+    this.tempPerson=[this.person[rando]];
+    console.log(rando, this.person)
   }
 }
-
-module.exports=this.person;
