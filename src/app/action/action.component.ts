537563cf2222b81f32acf96d5474fa8751e57d20
TOBY PETERSON <tspeterson206@gmail.com>
2020-03-12 23:25:02 -0700

    added addnoun and dropdown form

--- src/app/action/action.component.ts
+++ src/app/action/action.component.ts
@@ -31,8 +31,6 @@ const colors = ['yellow','green','blue','orange','black','red','purple','lightbl
   ]
 })
 export class ActionComponent implements OnInit {
-  // action=['put flouride in our drinking water in order to control out minds','was responsible for  9/11','is the orchestrator of pizzagate','embezzles from the Boeing pension fund','can control the weather','has the key to entering the matrix','is actually the tooth fairy','invented the phrase "YOLO"','invented the internet','sells organs on the black market','is the real Dread Pirate Roberts','never learned to read','knows that Nicholas Cage actually did steal the declaration of independence','successfully cloned a congressman','is actually over 600 years old','plays poker with the devil','has danced with the devil in the pale moonlight','ghost wrote all of Shakespeare\'s plays','is actually a clone','has kept a tight lid on hydro-fusion','funds dark-web red rooms','is actually one of a set of triplets'];
-  // tempAction='';
 
   @Input() action: Array<string>;
   @Input() tempAction: string;
